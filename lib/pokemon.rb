246e6900d394ddb679f64b67458e62ef2663f521
arnie b <arnold.bwaila@gmail.com>
2020-03-16 11:17:50 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,27 +8,21 @@ class Pokemon
     @db = db
   end
   
-  def save(name, type, db)
-    if self.id
-      db.execute("UPDATE pokemon SET name = ? SET type = ? WHERE id = ? ",name,type,self.id)
-      self.name = name 
-      self.type = type
-      new_pokemon = self
-    else
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
-    @id = db.execute("SELECT last_insert_rowid() FROM students")[0][0]
-    new_pokemon = self.new(id: @id name: name, type: type, db: db)
-    end
-    new_pokemon
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)",name, type)
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
   
   def self.find(id, db)
     poke = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-    if poke.length >-1
+    if poke[0]
     name = poke[1]
     type = poke[2]
-    poke_found = Pokemon.new(id: id, name: name, type: type, db: db)
-    pokemon_found
+    pokef = self.new(id: id, name: name, type: type, db: db)
+    else
+      pokef = nil
+    end
+    pokef
  end
 
 end
