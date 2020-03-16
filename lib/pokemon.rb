b4b23daa9cd99cd9f6c61eec6f645812537e848d
arnie b <arnold.bwaila@gmail.com>
2020-03-16 09:49:32 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,32 +1,34 @@
 class Pokemon
   attr_accessor :id ,:name, :type, :db
   
-  
-<<<<<<< HEAD
   def initialize(id:, name:, type:, db:)
-=======
-  def initialize(id, name, type, db)
     @id = id
     @name = name 
     @type = type
     @db = db
   end
   
-<<<<<<< HEAD
-  def save(name:, type:, db:)
+  def save(name, type, db)
+    if self.id
+      db.execute("UPDATE pokemon SET name = ? SET type = ? WHERE id = ? ",name,type,self.id)
+      self.name = name 
+      self.type = type
+      new_pokemon = self
+    else
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+    @id = db.execute("SELECT last_insert_rowid() FROM students")[0][0]
+    new_pokemon = self.new(id: @id name: name, type: type, db: db)
+    end
+    new_pokemon
   end
   
-  def self.find(id, database_connection)
-    pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-    name = pokemon[1]
-    type = pokemon[2]
-    pokemon_found = Pokemon.new(id: id, name: name, type: type, db: database_connection)
+  def self.find(id, db)
+    poke = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    if poke.length >-1
+    name = poke[1]
+    type = poke[2]
+    poke_found = Pokemon.new(id: id, name: name, type: type, db: db)
     pokemon_found
  end
-=======
-  def save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
-  end
->>>>>>> 9108f607409cc98838c500adffab922037c192d3
+
 end
